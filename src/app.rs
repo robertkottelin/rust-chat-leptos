bde70fa83eb26ff6f4f4f1990f736babe97acf50
Robert Kottelin <robert.kottelin@gmail.com>
2023-05-01 21:04:59 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
-use std::{net::TcpStream, io::{Read, Write}};
+
 
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::WebSocket;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
@@ -17,7 +19,7 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
         // sets the document title
-        <Title text="Rust Chat Application"/>
+        <Title text="Welcome to Leptos"/>
 
         // content for this welcome page
         <Router>
@@ -30,46 +32,46 @@ pub fn App(cx: Scope) -> impl IntoView {
     }
 }
 
-// create function to connect to server and send data
-fn connect_to_server() {
-    // connect to tcp stream
-    let mut stream = TcpStream::connect("localhost:8081").unwrap();
-    // read from stream and collect into variable
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    // print out the buffer
-    println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
-}
-
-/// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let ws = WebSocket::new("ws://localhost:8080").unwrap();
 
-    // connect to tcp stream
-    let mut stream = TcpStream::connect("localhost:8081").unwrap();
-    // read from stream and collect into variable
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    // print out the buffer
-    println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
-    
-    // write to stream
-    stream.write(b"username \n").unwrap();
-    stream.flush().unwrap();
-    stream.write(b"password \n").unwrap();
-    stream.flush().unwrap();
-    stream.write(b"Hello from the client!! \n").unwrap();
-    stream.flush().unwrap();
+    let (chat_messages, set_chat_messages) = create_signal(cx, vec![]);
 
-    println!("Sent Hello, awaiting reply...");
+    let onmessage_callback = Closure::wrap(Box::new(move |msg: JsValue| {
+        let msg = msg.dyn_into::<web_sys::MessageEvent>().unwrap();
+        let message = msg.data().as_string().unwrap_throw();
+        set_chat_messages.update(|chat_messages| chat_messages.push(message));
+    }) as Box<dyn FnMut(JsValue)>);
 
-    view! { cx,
-        <h1>"Rust Chat Application"</h1>
-        // show the received data from the server
-        <p>{String::from_utf8_lossy(&buffer[..])}</p>
-        <button on:click=on_click></button>
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
+
+    let on_send = move |text: String| {
+        ws.send_with_str(&text).unwrap();
+    };
+
+    view! {
+        cx,
+        <div>
+            <form
+                on:submit=move |ev| {
+                    ev.prevent_default();
+                    let input = ev.target().unwrap().dyn_into::<web_sys::HtmlFormElement>().unwrap();
+                    let input_value = input.query_selector("#message-input").unwrap().unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value();
+                    on_send(input_value.clone());
+                    input.reset();
+                }
+            >
+                <label>
+                    "Message"
+                    <input type="text" name="message" id="message-input"/>
+                </label>
+                <input type="submit" value="Send"/>
+            </form>
+            <ul>
+                {chat_messages.get().iter().map(|msg| view! { cx, <li>{msg}</li> }).collect::<Vec<_>>()}
+            </ul>
+        </div>
     }
 }
